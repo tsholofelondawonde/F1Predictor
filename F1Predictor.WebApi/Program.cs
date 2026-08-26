01d068c89e5f98c061a0b44e295fdbbee35d436e
tsholofelondawonde <hopendawonde@gmail.com>
2026-08-26 16:26:45 +0200

    fix: update ForwardedHeadersOptions to clear KnownIPNetworks instead of KnownNetworks

--- F1Predictor.WebApi/Program.cs
+++ F1Predictor.WebApi/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddObservability(builder.Environment, builder.Configuration);
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    options.KnownNetworks.Clear();
+    options.KnownIPNetworks.Clear();
     options.KnownProxies.Clear();
 });
 
